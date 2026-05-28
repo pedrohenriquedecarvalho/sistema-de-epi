bd6857341ead938323cae55a8feb10caa9ccfdf2
pedrohenriquedecarvalho <pedrohecarvalho3@gmail.com>
2026-05-28 16:00:03 -0300

    ...

--- src/views/home.vue
+++ src/views/home.vue
@@ -60,29 +60,10 @@
       <div class="footer-content">
         
         <div class="footer-brand">
-          <img src="../assets/image.png" alt="Logo" class="footer-logo">
-          <p>&copy; 2026 Controle de EPIs.</p>
-        </div>
-        
-        <div class="links">
-          <h3>Navegação</h3>
-         <ul >
-          <li><RouterLink to="/" class="navbar__link">Home</RouterLink></li>
-          <li><RouterLink to="/cadastro" class="navbar__link">Cadastro</RouterLink></li>
-          <li><RouterLink to="/estoque" class="navbar__link">Estoque</RouterLink></li>
-          <li><RouterLink to="/login" class="navbar__link">Login</RouterLink></li>
-          <li><RouterLink to="/relatorio" class="navbar__link">Relatórios</RouterLink></li>
-          <li><RouterLink to="/relatorio" class="navbar__link">Relatórios</RouterLink></li>
-          <li><RouterLink to="/entrega" class="navbar__link">Entrega</RouterLink></li>
           
-</ul>
+          <p>&copy; Sistema de EPIs.</p>
         </div>
 
-        <div class="contato">
-          <h3>Suporte</h3>
-          <p>suporte@gestaoepi.com.br</p>
-          <p>(11) 4002-8922</p>
-        </div>
 
       </div>
     </footer>
@@ -213,39 +194,21 @@ export default {
 footer {
   background-color: #111827;
   color: #9ca3af;
-  padding: 60px 50px 30px;
+ 
   margin-top: 80px;
 }
 
 .footer-content {
   display: flex;
-  justify-content: space-between;
+  justify-content: center;
   gap: 40px;
 }
 
 .footer-logo {
-  height: 40px;
-  filter: brightness(0) invert(1);
+  height: 20px;
+ 
 }
 
-.links h3,
-.contato h3 {
-  color: white;
-  margin-bottom: 15px;
-}
-
-.links ul {
-  list-style: none;
-}
-
-.links a {
-  color: #9ca3af;
-  text-decoration: none;
-}
-
-.links a:hover {
-  color: white;
-}
 
 /* RESPONSIVO */
 @media (max-width: 768px) {
